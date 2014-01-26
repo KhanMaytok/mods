a9554f51f447b5d840e3e39db7d0e81f61eb5715
EmmaKurianova <holas001@latinmail.com>
2014-01-26 12:02:10 -0500

    Generic Commit

---
+++ jos.bat
@@ -0,0 +1,2 @@
+git config --global user.name "Giancarlo Ventura"
+git config --global user.email sakya_stelios@hotmail.com
---
+++ manda.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "Generic Commit"
+git push origin master
